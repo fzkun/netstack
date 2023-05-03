d36cee250645c9862fa1da657d4ced40ed482574
fzkun <412657308@qq.com>
2023-05-03 18:08:15 +0800

    去掉限制build

--- rand/rand.go
+++ rand/rand.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build !linux
+//go:build !linux
 
 // Package rand implements a cryptographically secure pseudorandom number
 // generator.
